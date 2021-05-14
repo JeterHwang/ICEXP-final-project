d914e248ed500ce29ff44b95e9ce043cbdacbfd0
JeterHwang <jeter0309@gmail.com>
2021-05-14 14:00:01 +0800

    update

--- syn/synthesis.tcl
+++ syn/synthesis.tcl
@@ -9,16 +9,16 @@ link
 
 ############in sdc file
 # Set the Optimization Constraints 
-create_clock -period 10 -name "clk_p_i" -waveform {0 5} "clk_p_i"
-set_dont_touch_network [get_ports clk_p_i]
-set_fix_hold [get_clocks clk_p_i]
+create_clock -period 10 -name "clk" -waveform {0 5} "clk"
+set_dont_touch_network [get_ports clk]
+set_fix_hold [get_clocks clk]
 
 
 # Define the design environment
-set_clock_uncertainty  0.1  [get_clocks clk_p_i]
-set_clock_latency      0.5  [get_clocks clk_p_i]
-set_input_delay -max 1 -clock clk_p_i [all_inputs]
-set_output_delay -min 0.5 -clock clk_p_i [all_outputs]
+set_clock_uncertainty  0.1  [get_clocks clk]
+set_clock_latency      0.5  [get_clocks clk]
+set_input_delay -max 1 -clock clk [all_inputs]
+set_output_delay -min 0.5 -clock clk [all_outputs]
 set_drive 1 [all_inputs]
 set_load  1 [all_outputs]
 
@@ -29,7 +29,7 @@ set_operating_conditions -min_library fsa0m_a_generic_core_ff1p98vm40c -min BCCO
 set_wire_load_model -name G200K -library fsa0m_a_generic_core_tt1p8v25c
 
 set_max_area 0
-set_max_fanout 6 alu
+set_max_fanout 6 SMVM
 set_boundary_optimization {"*"}
 #############in sdc file
 
